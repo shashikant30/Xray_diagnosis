b025c1eb3c8c17c94b1cb383356a110c06504c36
shashikant <shashikantilhe@gmail.com>
2022-03-14 11:14:55 +0530

    temporary commit to fix register function

--- Xray_diagnosis/urls.py
+++ Xray_diagnosis/urls.py
@@ -17,12 +17,16 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings  
 from django.conf.urls.static import static  
-from website.views import userFormView, resultView
+from website import views
 from website.forms import userForm
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', userFormView ),
-    path('result/', resultView)
+    path('userinfo/', views.userFormView, name='userFormView'),
+    path('', views.home, name='home'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('signout', views.signout, name='signout'),
+    path('result/', views.resultView)
 ]
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL,
